3f88297ca40bb6f3feca5df94dc11593898ae1b7
cdylpp <cdylpp@yahoo.com>
2026-05-15 21:22:08 -0700

    initial commit

---
+++ tests/simple.jl
@@ -0,0 +1,8 @@
+function myFunc(x, y)
+    return sum(x, y)
+end
+
+function echo(s)
+    println(s)
+end
+
---
+++ tests/simple.py
@@ -0,0 +1,19 @@
+def myFunc(x, y, z):
+    return sum(x, y, z)
+
+
+x = "123"
+
+def f(x, y):
+    if x == y:
+        return "they are equal"
+    elif x > y:
+        return "x is bigger"
+    else:
+        return "x is smaller"
+    
+
+# def has diff struct.
+
+inc = lambda x: sum(x,1)
+
